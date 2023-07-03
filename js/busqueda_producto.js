7e95e0c565d3040fed752a89c63c4581d9119bf9
Ramses <127460936+ramsesgithub@users.noreply.github.com>
2023-07-03 14:08:36 -0300

    busqueda error

---
+++ js/busqueda_producto.js
@@ -0,0 +1,13 @@
+// const inputs = document.getElementById("buscar");
+// const card = document.querySelectorAll(".card");
+
+// const filtroBusqueda = (input, selector)=>{
+//     inputs.addEventListener("keyup", e =>{
+//         if(e.key==="Escape") e.target.value="";
+//         document.querySelectorAll(selector).forEach(element => {
+//             element.textContent.toLowerCase().includes(e.target.value) ? el.classList.remove("filter") : el.classList.add("filter")
+//         })
+//     })
+// }
+// console.log(card);
+// filtroBusqueda(inputs,card);
