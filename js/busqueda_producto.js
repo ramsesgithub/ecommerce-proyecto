23b3a1ba4f47bfd07efe22934160a900888247a4
Ramses <127460936+ramsesgithub@users.noreply.github.com>
2023-07-04 11:27:40 -0300

    responsive index y pagina 404

--- js/busqueda_producto.js
+++ js/busqueda_producto.js
@@ -1,14 +1,14 @@
-const inputs = document.getElementById("buscar");
+const inputs = document.querySelectorAll(".buscar");
 
 const filtroBusqueda = (input, selector)=>{
-    inputs.addEventListener("keyup", e =>{
+    inputs.forEach(e=>e.addEventListener("keyup", e =>{
         if(e.key==="Escape") e.target.value="";
         document.querySelectorAll(selector).forEach(element => {
             element.textContent.toLowerCase().includes(e.target.value) 
             ? element.classList.remove("filter") 
             : element.classList.add("filter")
         })
-    })
+    }))
 }
 
 filtroBusqueda(".header__container-input",".card");
