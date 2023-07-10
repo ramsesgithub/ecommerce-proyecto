5c21576297caaa1df07eb53028417a7f841093a5
Ramses <127460936+ramsesgithub@users.noreply.github.com>
2023-07-10 11:00:25 -0300

    modificando login y footer

--- controllers/login_service.js
+++ controllers/login_service.js
@@ -1,20 +1,15 @@
 import { productServices } from "../services/product-service.js";
 
 const form = document.querySelector(".login__form");
+//res del resultado de email
+let res;
 
 const mostrarClientes = async (emailUsuario, passwordUsuario) => {
     const data = await productServices.listaPerfil();
     data.forEach(({email, password}) => {
         if(email === emailUsuario && password === passwordUsuario){
-            Swal.fire({
-                position: 'top',
-                icon: 'success',
-                title: 'Iniciando Sesion...',
-                showConfirmButton: false,
-                timer: 2000,
-            }).then(function() {
-                window.location.href = "../pages/admin_productos.html";
-            });
+            //si el resultado de email, emailusu y pass, passUsu son iguales entonces res vale true;
+            res = true;
         }
     })
 }
@@ -23,15 +18,30 @@ const mostrarClientes = async (emailUsuario, passwordUsuario) => {
 
 form.addEventListener("submit", e =>{
     e.preventDefault();
+    //se guarda el valor que ingreso el usuario en el form
     let emailUsuario = document.getElementById("email").value;
     let passwordUsuario = document.getElementById("password").value;
-    if((mostrarClientes(emailUsuario, passwordUsuario))){
-        Swal.fire({
-            position: 'top',
-            icon: 'error',
-            title: 'Email y/o contraseña incorrecto',
-            showConfirmButton: false,
-            timer: 1500,
-        })
-    }
-})
+    mostrarClientes(emailUsuario, passwordUsuario).then(()=>{
+        //Comparamos el valor de res si es true, iniciamos Sesion
+        if(res){
+            Swal.fire({
+                position: 'top',
+                icon: 'success',
+                title: 'Iniciando Sesion...',
+                showConfirmButton: false,
+                timer: 2000,
+            }).then(function(){
+                location.href="../pages/admin_productos.html";
+            })
+        }else{
+            Swal.fire({
+                position: 'top',
+                icon: 'error',
+                title: 'Email y/o contraseña incorrecto',
+                showConfirmButton: false,
+                timer: 1500,
+            })
+        }
+    })
+});
+
