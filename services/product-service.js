c03e923da8cb5b944403729c40b581bcf7733ebf
Ramses <127460936+ramsesgithub@users.noreply.github.com>
2023-07-11 11:20:35 -0300

    modificando js

--- services/product-service.js
+++ services/product-service.js
@@ -1,11 +1,11 @@
 const listaProductos = async () => {const data = await fetch("https://apirestone.onrender.com/productos"); return data.json();} 
 const listaPerfil = async () => {const data = await fetch("https://apirestone.onrender.com/perfil"); return data.json();} 
-const verProducto = async (id) => {const data = await fetch(`https://apirestone.onrender.com/productos/${id}`); return data.json();} 
+const verProducto = async id => {const data = await fetch(`https://apirestone.onrender.com/productos/${id}`); return data.json();} 
 
 
 
-const crearProducto = (nombre, precio, descripcion, imagen, categoria) => {
-    return fetch("https://apirestone.onrender.com/productos",{
+const crearProducto = async (nombre, precio, descripcion, imagen, categoria) => {
+    return await fetch("https://apirestone.onrender.com/productos",{
         method:"POST",
         headers:{
             "Content-Type": "application/json"
@@ -15,28 +15,26 @@ const crearProducto = (nombre, precio, descripcion, imagen, categoria) => {
 }
 
 
-const eliminarProducto = (id) => {
-    return fetch(`https://apirestone.onrender.com/productos/${id}`, {
+const eliminarProducto = async id => {
+    return await fetch(`https://apirestone.onrender.com/productos/${id}`, {
     method: "DELETE"
     });
 };
 
 
-const detalleProducto = id => {
-    return fetch(`https://apirestone.onrender.com/productos/${id}`).then(responde => responde.json())
+const detalleProducto = async id => {
+    return await fetch(`https://apirestone.onrender.com/productos/${id}`).then(responde => responde.json())
 }
 
 
-const editarProducto = (nombre, precio, descripcion, imagen, id, categoria) => {
-    return fetch(`https://apirestone.onrender.com/productos/${id}`,{
+const editarProducto = async (nombre, precio, descripcion, imagen, id, categoria) => {
+    return await fetch(`https://apirestone.onrender.com/productos/${id}`,{
         method:"PUT",
         headers:{
             "Content-Type":"application/json"
         },
         body:JSON.stringify({nombre, precio, descripcion, imagen, categoria})
     })
-    .then(responde => console.log(responde))
-    .catch(error => console.log(error))
 }
 
 
