30d028da5277b0437991591dae7fadc7f023f438
ramsesgithub <127460936+ramsesgithub@users.noreply.github.com>
2023-07-13 20:49:31 -0300

    modificando url API

--- services/product-service.js
+++ services/product-service.js
@@ -1,11 +1,11 @@
-const listaProductos = async () => {const data = await fetch("https://apirestone.onrender.com/productos"); return data.json();} 
-const listaPerfil = async () => {const data = await fetch("https://apirestone.onrender.com/perfil"); return data.json();} 
-const verProducto = async id => {const data = await fetch(`https://apirestone.onrender.com/productos/${id}`); return data.json();} 
+const listaProductos = async () => {const data = await fetch("https://64b07c10c60b8f941af5bd9e.mockapi.io/producto"); return data.json();} 
+const listaPerfil = async () => {const data = await fetch("https://64b07c10c60b8f941af5bd9e.mockapi.io/perfil"); return data.json();} 
+const verProducto = async id => {const data = await fetch(`https://64b07c10c60b8f941af5bd9e.mockapi.io/producto/${id}`); return data.json();} 
 
 
 
 const crearProducto = async (nombre, precio, descripcion, imagen, categoria) => {
-    return await fetch("https://apirestone.onrender.com/productos",{
+    return await fetch("https://64b07c10c60b8f941af5bd9e.mockapi.io/producto",{
         method:"POST",
         headers:{
             "Content-Type": "application/json"
@@ -16,19 +16,19 @@ const crearProducto = async (nombre, precio, descripcion, imagen, categoria) => 
 
 
 const eliminarProducto = async id => {
-    return await fetch(`https://apirestone.onrender.com/productos/${id}`, {
+    return await fetch(`https://64b07c10c60b8f941af5bd9e.mockapi.io/producto/${id}`, {
     method: "DELETE"
     });
 };
 
 
 const detalleProducto = async id => {
-    return await fetch(`https://apirestone.onrender.com/productos/${id}`).then(responde => responde.json())
+    return await fetch(`https://64b07c10c60b8f941af5bd9e.mockapi.io/producto/${id}`).then(responde => responde.json())
 }
 
 
 const editarProducto = async (nombre, precio, descripcion, imagen, id, categoria) => {
-    return await fetch(`https://apirestone.onrender.com/productos/${id}`,{
+    return await fetch(`https://64b07c10c60b8f941af5bd9e.mockapi.io/producto/${id}`,{
         method:"PUT",
         headers:{
             "Content-Type":"application/json"
